cf530215c0c3a1910777c4348ccb0dcce0043f6f
ChunyuBai <baichunyu26@gmail.com>
2022-10-21 15:24:39 -0400

    add new function hello.js file

---
+++ hello.js
@@ -0,0 +1,17 @@
+/*const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();*/
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("cali");
+sayHello("Mira");
+sayHello("Ferd");
+
+/*Hello, cali
+Hello, Mira
+Hello, Ferd*/
+
